b7fca5274c6b4c61685b895b0ec4095786fcf97e
WanWizard <wanwizard@wanwizard.eu>
2013-07-24 16:20:33 +0200

    added bootstrap support; added ServicesProvider

---
+++ src/Fuel/Alias/ServicesProvider.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package    Fuel\Alias
+ * @version    2.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Fuel\Alias;
+
+use Fuel\Dependency\ServiceProvider;
+
+/**
+ * ServicesProvider class
+ *
+ * Defines the services published by this namespace to the DiC
+ *
+ * @package  Fuel\Alias
+ *
+ * @since  1.0.0
+ */
+class ServicesProvider extends ServiceProvider
+{
+	/**
+	 * @var  array  list of service names provided by this provider
+	 */
+	public $provides = array('alias');
+
+	/**
+	 * Service provider definitions
+	 */
+	public function provide()
+	{
+		// \Fuel\Alias\Manager
+		$this->register('alias', function ($dic)
+		{
+			return new Manager;
+		});
+	}
+}
---
+++ src/bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package    Fuel\Alias
+ * @version    2.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Fuel\Alias;
+
+/**
+ * FuelPHP Composer library framework bootstrap
+ */
+
+// register the services of this libr
+\Fuel::getDic()->registerService(new ServicesProvider);
+
+/**
+ * FuelPHP Composer library application bootstrap
+ */
+return function($app) {
+
+	// your app initialisation code here
+};
